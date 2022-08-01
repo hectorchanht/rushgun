c336eba5985c34a35ff99ac4c27f4105b1b72944
hectorchanht <hectorchanht@gmail.com>
2022-08-02 00:42:28 +0800

    Update gun.js

--- src/libs/gun.js
+++ src/libs/gun.js
@@ -2,7 +2,7 @@ import Gun from 'gun/gun';
 import 'gun/sea';
 
 
-const gun = Gun({
+const gun = new Gun({
   // peers: ['https://gun-manhattan.herokuapp.com/gun'],
   // file: 'file/path.json'
 });
