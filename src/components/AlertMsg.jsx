739473d83221a04b8191bf389c4ebd4e2e580065
hectorchanht <hectorchanht@gmail.com>
2022-08-03 02:07:21 +0800

    remove AlertShadow="lg"

--- src/components/AlertMsg.jsx
+++ src/components/AlertMsg.jsx
@@ -11,7 +11,7 @@ const AlertMsg = ({ msg, setMsg }) => {
   if (!msg) return null;
 
   return <Alert alignItems='center' justifyContent='center' bg={'#AB47BC'}
-    borderRadius={8} AlertShadow="lg" position={'fixed'} bottom={'24px'} left={'0'}
+    borderRadius={8} position={'fixed'} bottom={'24px'} left={'0'}
     textAlign='center' status="error">
     {msg}
   </Alert>;
